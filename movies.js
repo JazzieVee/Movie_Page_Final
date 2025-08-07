8930b30e6606e56aec3c88c9a0fa22b736ec083e
Jasmine <oatman.estates.holdings@gmail.com>
2025-08-07 11:37:40 -0400

    additions

--- movies.js
+++ movies.js
@@ -1,13 +1,13 @@
 //  http://www.omdbapi.com/?apikey=5c7ce648
 
-const movieListEl = document.querySelector('movie__results');
+const movieListEl = document.querySelector('#movie__results');
 
      async function main () {
      const result = await fetch(`https://www.omdbapi.com/?apikey=5c7ce648&s=${''}`)
      const data = await result.json();
    
      movieListEl.innerHTML = data.Search.map((movie) => 
-     console.log(movieHTML(movie))).join("");
+     movieHTML(movie)).join("");
      } 
 
      main();
@@ -15,12 +15,12 @@ const movieListEl = document.querySelector('movie__results');
     function movieHTML (movie) {
     return `<div class="movie__content">
     <figure class="movie__poster">
-        <img src="${movie.Search.Poster}" alt="" class="poster">
+        <img src="${movie.Poster}" alt="" class="poster">
     </figure>
        <div class="movie__description">
-        <h4 class="movie__title">${movie.Search.Title}</h4>
-        <p class="movie__genre">${movie.Search.Genre}</p>
-        <p class="movie__runtime">${movie.Search.Runtime}</p>
+        <h4 class="movie__title">${movie.Title}</h4>
+        <p class="movie__genre">${movie.Genre}</p>
+        <p class="movie__runtime">${movie.Runtime}</p>
     </div>
     </div>
      `
